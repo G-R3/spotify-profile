d69d20fdc15f8e84ec1deca29a0dd095a15e02c1
Gerardo <gr261212@gmail.com>
2022-01-17 22:35:11 -0500

    display users top tracks

---
+++ src/components/Tracks.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { getTopTrack } from "../api";
+import { milliToMinutesAndSeconds } from "../utils/utils";
+
+export default function Tracks() {
+    const [topTracks, setTopTracks] = useState("");
+
+    useEffect(() => {
+        const fetchTracks = async () => {
+            const topTrackData = await getTopTrack();
+            setTopTracks(topTrackData);
+        };
+
+        fetchTracks();
+    }, []);
+
+    return topTracks ? (
+        <div>
+            <h2 className="text-lg font-bold">Top Tracks</h2>
+            <div className="">
+                {topTracks.items.map((track, i) => (
+                    <div key={i} className="w-[400px] flex gap-4 text-sm my-5">
+                        <img
+                            src={track.album.images[0].url}
+                            alt=""
+                            className="w-12 h-w-12"
+                        />
+                        <div className="w-full">
+                            <h3 className="flex justify-between">
+                                {track.name}
+                                <span className=" text-neutral-400">
+                                    {milliToMinutesAndSeconds(
+                                        track.duration_ms,
+                                    )}
+                                </span>
+                            </h3>
+                            <span className=" text-neutral-400">
+                                {track.artists[0].name}
+                            </span>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    ) : (
+        <p>Loading...</p>
+    );
+}
--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import NavBar from "./NavBar";
+import NavBar from "./Sidebar";
+import Tracks from "./Tracks";
 import { getUser, getFollowing } from "../api";
 
 export default function User() {
@@ -10,29 +11,53 @@ export default function User() {
         const fetchUser = async () => {
             const data = await getUser();
             const followingData = await getFollowing();
+
             setUser(data);
             setFollowing(followingData);
         };
 
         fetchUser();
     }, []);
-
     return (
         user && (
-            <div className="">
+            <div>
                 <NavBar />
-                <div className="ml-52 py-10 flex flex-col items-center">
-                    <img
-                        src={user.images[0].url}
-                        alt=""
-                        className="w-52 h-52 rounded-full"
-                    />
-                    <h1>Welcome {user.display_name}</h1>
-                    <span>
-                        Followers {user.followers.total} | Following
-                        {following.total}
-                    </span>
-                </div>
+                <main className="ml-52 py-10 ">
+                    <div className="flex flex-col gap-16">
+                        <section className="flex flex-col items-center gap-2">
+                            <img
+                                src={user.images[0].url}
+                                alt=""
+                                className="w-52 h-52 rounded-full"
+                            />
+                            <h1 className="my-3 font-semibold">
+                                Welcome @{user.display_name}
+                            </h1>
+                            <div className="flex justify-center items-center gap-3 h-10">
+                                <div className="bg-neutral-800 rounded-md py-1 px-4">
+                                    <p className="text-neutral-400 text-sm font-semibold">
+                                        Followers
+                                    </p>
+                                    <span className="text-spotify-green text-center block font-bold">
+                                        {user.followers.total}
+                                    </span>
+                                </div>
+                                <div className="bg-neutral-800 rounded-md py-1 px-4">
+                                    <p className="text-neutral-400 text-sm font-semibold">
+                                        Following
+                                    </p>
+                                    <span className="text-spotify-green text-center block font-bold">
+                                        {following.total}
+                                    </span>
+                                </div>
+                            </div>
+                        </section>
+                        <section className="flex justify-evenly">
+                            <Tracks />
+                            <Tracks />
+                        </section>
+                    </div>
+                </main>
             </div>
         )
     );
