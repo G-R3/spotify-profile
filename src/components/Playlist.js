290da88419b031e1db5467b8bf6a530811c15bf8
Gerardo <gr261212@gmail.com>
2022-01-24 22:29:27 -0500

    added external links to album and artists of a song

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -51,42 +51,60 @@ export default function Playlist() {
 
             <div className="flex flex-col gap-2">
                 {playlist.tracks.items.map((item, i) => (
-                    <a
-                        href={item.track.uri}
+                    <div
                         key={i}
-                        className="hover:bg-neutral-800 rounded-md"
+                        className="h-16 p-3 flex items-center justify-between gap-4 text-sm hover:bg-neutral-800 rounded-md group"
                     >
-                        <div className="h-16 p-3 flex items-center justify-between gap-4 text-sm">
-                            <div className="flex gap-5 items-center">
-                                <span className="text-neutral-400 font-semibold text-xs">
-                                    {i + 1}
-                                </span>
-                                <img
-                                    src={item.track.album.images[0]?.url}
-                                    alt=""
-                                    className="w-10 h-10"
-                                />
-                                <div className="">
-                                    <h3 className="flex justify-between min-w-[25ch] w-[35ch]">
-                                        <span className="truncate">
-                                            {item.track.name}
-                                        </span>
-                                    </h3>
-                                    <span className="text-xs text-neutral-400">
-                                        {item.track.artists[0].name}
+                        <div className="flex gap-5 items-center">
+                            <span className="text-neutral-400 font-semibold text-xs">
+                                {i + 1}
+                            </span>
+                            <img
+                                src={item.track.album.images[0]?.url}
+                                alt=""
+                                className="w-10 h-10"
+                            />
+                            <div className="">
+                                <h3 className="flex justify-between min-w-[25ch] w-[35ch]">
+                                    <span className="truncate">
+                                        {item.track.name}
                                     </span>
-                                </div>
+                                </h3>
+                                <span className="text-xs text-neutral-400 hover:underline hover:underline-offset-1 group-hover:text-white">
+                                    {item.track.artists[0].external_urls
+                                        .spotify ? (
+                                        <a
+                                            href={
+                                                item.track.artists[0]
+                                                    .external_urls.spotify
+                                            }
+                                            target={"_blank"}
+                                            rel="noreferrer"
+                                        >
+                                            {item.track.artists[0].name}
+                                        </a>
+                                    ) : (
+                                        item.track.artists[0].name
+                                    )}
+                                </span>
                             </div>
-                            <span className="min-w-[25ch] w-[35ch] truncate text-xs text-neutral-400">
-                                {item.track.album.name}
-                            </span>
-                            <span className=" text-neutral-400">
-                                {milliToMinutesAndSeconds(
-                                    item.track.duration_ms,
-                                )}
-                            </span>
                         </div>
-                    </a>
+                        <span
+                            className="min-w-[25ch] w-[35ch] truncate text-xs text-neutral-400 hover:underline 
+                        hover:text-white hover:underline-offset-1"
+                        >
+                            <a
+                                href={item.track.album.external_urls.spotify}
+                                target={"_blank"}
+                                rel="noreferrer"
+                            >
+                                {item.track.album.name}
+                            </a>
+                        </span>
+                        <span className=" text-neutral-400">
+                            {milliToMinutesAndSeconds(item.track.duration_ms)}
+                        </span>
+                    </div>
                 ))}
             </div>
         </div>
