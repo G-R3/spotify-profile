ec649c3978337955f17e40a5bb8910ba230ba5a0
Gerardo <gr261212@gmail.com>
2022-03-04 17:49:24 -0500

    add image dominant color

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,12 +4,14 @@ import UserTopArtists from "./UserTopArtists";
 import { getUserData } from "../api";
 import Loader from "./Loader";
 import { Link } from "react-router-dom";
+import getImageColor from "../utils/imageColor";
 
 export default function User() {
     const [user, setUser] = useState("");
     const [following, setFollowing] = useState("");
     const [topTracks, setTopTracks] = useState("");
     const [topArtists, setTopArtists] = useState("");
+    const [imageColor, setImageColor] = useState("");
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -25,11 +27,28 @@ export default function User() {
         fetchUser();
     }, []);
 
+    useEffect(() => {
+        if (!user) {
+            return;
+        }
+        const getColor = async () => {
+            let color = await getImageColor(user.images[0].url);
+            setImageColor(color);
+        };
+
+        getColor();
+    }, [user]);
+
     if (!user || !following || !topTracks || !topArtists) return <Loader />;
 
     return (
         <div className="py-10 px-5 flex flex-col gap-16 max-w-7xl mx-auto">
-            <section className="flex flex-col items-center sm:flex-row sm:items-end gap-8">
+            <section
+                style={{
+                    backgroundColor: imageColor,
+                }}
+                className="rounded-md py-20 px-10 flex flex-col items-center sm:flex-row sm:items-end gap-8 bg-gradient-to-t from-neutral-800"
+            >
                 <img
                     src={user.images[0].url}
                     alt=""
