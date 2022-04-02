659492d124416752e0f1352c412a98aba4350134
Gerardo <gr261212@gmail.com>
2022-04-02 12:14:35 -0400

    fixed layout for artist albums

--- src/components/ArtistProfile.js
+++ src/components/ArtistProfile.js
@@ -108,11 +108,18 @@ export default function ArtistProfile() {
                             className="flex flex-col items-center py-5 px-2 rounded-md text-sm bg-neutral-900 shadow-lg hover:bg-neutral-800 transition-all group"
                         >
                             <div className="flex flex-col gap-5">
-                                <img
-                                    src={album.images[0]?.url}
-                                    alt=""
-                                    className="w-48 h-48 rounded-md group-hover:shadow-lg"
-                                />
+                                <div className="relative mb-4">
+                                    <div className="relative pb-[100%] w-full">
+                                        <div>
+                                            <img
+                                                src={album.images[0]?.url}
+                                                alt=""
+                                                className="rounded-md block h-full lef-0 top-0 absolute w-full group-hover:shadow-lg"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <div>
                                     <h2 className="font-bold truncate w-[25ch]">
                                         {album.name}
