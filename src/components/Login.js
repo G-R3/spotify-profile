6be364df0dc3d075bb829447936a4ddd4f91e2e2
Gerardo <gr261212@gmail.com>
2022-04-16 13:29:22 -0400

    minor change to login page styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Login() {
     return (
         <div className="flex flex-col justify-center items-center h-full gap-8">
-            <h1 className="text-5xl font-bold">Spotify Stats</h1>
+            <h1 className="text-5xl font-bold text-center">Spotify Profile</h1>
             <a
                 href="http://localhost:3001/login"
                 className="bg-spotify-green text-white p-5 text-lg tracking-wide font-semibold rounded-full hover:-translate-y-1 transition-transform"
             >
-                LOGIN WITH SPOTIFY
+                Login with Spotify
             </a>
         </div>
     );
