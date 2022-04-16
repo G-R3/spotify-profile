161d2012b15a8b53e2dd7294ec44dd4f08f2f8f9
Gerardo <gr261212@gmail.com>
2022-04-16 11:57:07 -0400

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="flex justify-center px-5 py-10 text-neutral-400">
+            <p>Made with 💓 by Gerardo Rodriguez</p>
+        </footer>
+    );
+}
